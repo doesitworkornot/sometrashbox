b0076964f3aee60530c8aa1e90eb953cd62b20d1
doesitworkornot <60290354+doesitworkornot@users.noreply.github.com>
2020-01-29 17:09:38 +0800

    yeah
    so much changes

---
+++ serialread/serialbegin/src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+int val = 0;
+int led = 2; // пин лампочки
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);
+
+}
+
+void loop()
+ {
+  if (Serial.available()>0)
+  {
+    val = Serial.read();
+    Serial.println(val);
+    digitalWrite(led,HIGH);
+    delay(400);
+    digitalWrite(led,LOW);
+    Serial.flush();
+  }
+ }
